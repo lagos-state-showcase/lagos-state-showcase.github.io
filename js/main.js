ac801db4fd4d76bec350cd7d4d09f64575b9911b
NATFINEST <ese.nat4@gmail.com>
2021-05-24 23:52:39 +0100

    up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 	$('.callMap').select2();
 	$('.gm-style-iw-c .button').html('jvhvj');
-    $('.pgwSlideshow').pgwSlideshow();
+    $('.pgwSlideshow').pgwSlideshow({
+    	maxHeight: 600
+    });
+
+    $("a").on('click' ,function(e){
+    	if($(this).attr('href')=="#"){
+    		e.preventDefault();
+    	}
+    })
 })
+
